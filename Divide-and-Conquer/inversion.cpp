179790ddfeb219035b2b374d01be63c1f67bce71
Neeraj Deshpande <31987159+NKH123@users.noreply.github.com>
2019-04-07 11:24:46 +0530

    Fixed bug

--- Divide-and-Conquer/inversion.cpp
+++ Divide-and-Conquer/inversion.cpp
@@ -1,15 +1,69 @@
 #include<bits/stdc++.h>
-#define trace(x) cout<<#x<<" = "<<x<<"\n"
 using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <b; i++)
+typedef long long ll;
+#define int ll
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef pair<int,int> pi;
+#define trace(x) cerr<<#x<<"="<<x<<"\n";
+#define llp 1000000007
+#define mod 1000000007
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+	return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+	os << "{";
+	typename vector< T > :: const_iterator it;
+	for( it = v.begin(); it != v.end(); it++ ) {
+		if( it != v.begin() ) os << ", ";
+		os << *it;
+	}
+	return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+	os << "[";
+	typename set< T > :: const_iterator it;
+	for ( it = v.begin(); it != v.end(); it++ ) {
+		if( it != v.begin() ) os << ", ";
+		os << *it;
+	}
+	return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+	os << "[";
+	typename map< F , S >::const_iterator it;
+	for( it = v.begin(); it != v.end(); it++ ) {
+		if( it != v.begin() ) os << ", ";
+		os << it -> first << " = " << it -> second ;
+	}
+	return os << "]";
+}
+
+#define deb(x) cerr << #x << " = " << x << endl;
 vector<int>a;
+int cans;
 
 void combine(int l,int mid,int r){
+	cans=0;
 	vector<int>b;
 	int I=0;
 	b.resize(mid-l+1+r-(mid+1)+1);
 	int L=l; int R=mid+1;
 	while(L<=mid and R<=r){
-		if(a[L]<a[R]){
+		if(a[L]<=a[R]){
 			b[I]=a[L];
 			L++;
 			I++;
@@ -18,6 +72,7 @@ void combine(int l,int mid,int r){
 			b[I]=a[R];
 			R++;
 			I++;
+			cans+=(mid-L+1);
 		}
 	}
 	while(L<=mid){
@@ -36,43 +91,32 @@ void combine(int l,int mid,int r){
 }
 
 int  solve(int l,int r){
-	trace(l);
-	trace(r);
 	if(l==r){
 		return 0;
 	}
 	int mid=(l+r)/2;
-	trace(mid);
+	// trace(mid);
 	int lans=solve(l,mid);
 	int rans=solve(mid+1,r);
-	int cans=0;
-	trace(lans);
-	trace(rans);
-	int i;
-	int f=0;
-	for(i=0;i<=mid;i++){
-		if(a[i]>a[mid+1]){
-			f=1;
-			break;
-		}
-	}
-	if(f==1){
-		cans=(mid-i+1)*(r-(mid+1)+1);
-	}
-	trace(cans);
-	//combine
 	combine(l,mid,r);
-	trace(lans+rans+cans);
-
 	return (lans+rans+cans);
 }
 
-int main(){
-	int n;
-	cin>>n;
-	a.resize(n);
-	for(int i=0;i<n;i++)cin>>a[i];
-	int ans=solve(0,n-1);
-	cout<<ans<<"\n";
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		a.resize(n);
+		for(int i=0;i<n;i++)cin>>a[i];
+			int ans=solve(0,n-1);
+		cout<<ans<<"\n";
+	}
+
+
+
 	return 0;
 }
